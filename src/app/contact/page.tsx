a52f4904e550cffa9a79a9223deca23f48b84540
Gaston Saavedra <gaston.saavedra85@gmail.com>
2024-02-05 17:36:43 +0100

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact : React.FC<{children: string, className: string}> = (children, className) => {
+		return (
+				<div>
+						<h1>About</h1>
+				</div>
+		)
+}
+
+export default Contact;
